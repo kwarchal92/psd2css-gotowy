f8e7771e0bcb6ed16ae4679386aede4dd3ff0538
kwarchal92 <68201514+kwarchal92@users.noreply.github.com>
2021-01-17 19:18:57 +0100

    pod validator 3wc

---
+++ js/main.js
@@ -0,0 +1,21 @@
+
+let box = document.getElementsByClassName(`list`).lenght;
+
+
+const next = () => {
+
+}
+
+
+
+const back = () => {
+
+}
+
+
+let btnNext = document.getElementById(`next`);
+let btnBack = document.getElementById(`back`);
+
+
+btnNext.addEventListener(`click`, next);
+btnBack.addEventListener(`click`, back);
